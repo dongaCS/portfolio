a72195fc5b9a0a7169c16a9ed55d3490cad42cc8
dongaCS <109496734+dongaCS@users.noreply.github.com>
2024-08-24 03:19:23 -0400

    added selection of card images for pokeDex project

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const projectCards = document.querySelectorAll('.project-card');
+const pokeImg = document.getElementById('pokeDex-img')
 
 // flip card
 projectCards.forEach(card => {
@@ -9,3 +10,10 @@ projectCards.forEach(card => {
         }
     });
 });
+
+// random pokedex image card
+const options = ["red", "blue", "yellow", "green", "black", "brown", "purple", "gray", "white", "pink"];
+document.addEventListener('DOMContentLoaded', _ => {
+    const r = ~~(Math.random() * options.length);
+    document.getElementById('pokeDex-img').src = `./images/pokemon/${options[r]}.png`;
+  });
